60aa32624ea97bb6841ee66f63aa7231a934ce5d
ngs0908 <muneyuki9296@gmail.com>
2023-06-22 21:45:54 +0900

    プロトタイプを投稿できる

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,14 +1,20 @@
 class PrototypesController < ApplicationController
   def index
+    @prototype = Prototype.all  
   end
 
   def new
     @prototype = Prototype.new
   end
 
+  def create
+    Prototype.create(prototype_params)
+    redirect_to root_path
+  end
+
   private
 
   def prototype_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end
