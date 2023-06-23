337dcd6179873645ddf657745b48a13493a3089d
yauyouyou <iwaiwa1115@gmail.com>
2023-06-23 16:28:44 +0900

    一覧表示final2

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,8 @@
 class Prototype < ApplicationRecord
   has_one_attached :image
 
+  belongs_to :user
+
   validates :title, presence: true
   validates :catch_copy, presence: true
   validates :concept, presence: true
