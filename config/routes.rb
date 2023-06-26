52868f3612ab555df31b431dc04c04d375d7c56f
yauyouyou <iwaiwa1115@gmail.com>
2023-06-26 12:51:17 +0900

    ルーティングネスト設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
   resources :users
-  resources :prototypes
-  resources :comments
+  resources :prototypes do
+    resources :comments
+  end
 end
